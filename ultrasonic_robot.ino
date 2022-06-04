1fd212ab62d63506aa27050ac98b48d2618e9c3d
Murillo Martins <murillomartins@uol.com.br>
2022-06-04 19:32:06 -0300

    ultrassonics working

--- ultrasonic_robot.ino
+++ ultrasonic_robot.ino
@@ -4,24 +4,25 @@ PARA GIRAR O MOTOR NO SENTIDO ANTI-HORARIO A ENTRADA 1 DEVE ESTAR LOW ENQUANTO A
 PARA TRAVAR O MOTOR AS DUAS ENTRADAS DEVEM ESTAR LOW
 PARA FREIAR O MOTOR AS DUAS ENRTADAS DEVEM ESTAR HIGH
 */
+
 // Adicionando a biblioteca do sensor ultrassônico
 #include <Ultrasonic.h>
 
 // Definindo os sensores ultrassônicos
-Ultrasonic ultrasonic01(13, 12);
-Ultrasonic ultrasonic02(11, 10);
-Ultrasonic ultrasonic03(9, 8);
+Ultrasonic ultrasonic01(10, 9);
+Ultrasonic ultrasonic02(8, 7);
+Ultrasonic ultrasonic03(6, 5);
 
 // Definindo portas dos motores A e B 
-int IN_A1 = 4;
-int IN_A2 = 5;
-int IN_B1 = 6;
-int IN_B2 = 7;
+int IN_A1 = 11;
+int IN_A2 = 12;
+int IN_B1 = 3;
+int IN_B2 = 4;
 
 void setup() {
   // Inicia a porta serial
   Serial.begin(9600);
-
+  
   // Define pinos dos motores
   pinMode(IN_A1, OUTPUT);
   pinMode(IN_A2, OUTPUT);
@@ -30,11 +31,46 @@ void setup() {
 }
 
 void loop() {
-  // https://www.arduino.cc/reference/pt/language/structure/control-structure/dowhile/
-  do {
+  int ult01 = ultrasonic01.convert(ultrasonic01.timing(), Ultrasonic::CM);
+  int ult02 = ultrasonic02.convert(ultrasonic02.timing(), Ultrasonic::CM);
+  int ult03 = ultrasonic03.convert(ultrasonic03.timing(), Ultrasonic::CM);
+
+  /*
+  Serial.print("ULT 01: ");
+  Serial.println(ult01);
+  Serial.print("ULT 02: ");
+  Serial.println(ult02);
+  Serial.print("ULT 03: ");
+  Serial.println(ult03);
+  */
+  
+  if (ult01 > 10) {
+    // Robô anda para frente se não houver nenhum obstáculo na frente do robô
+    int ult01 = ultrasonic01.convert(ultrasonic01.timing(), Ultrasonic::CM);
     digitalWrite(IN_A1, HIGH);
     digitalWrite(IN_A2, LOW);
-    digitalWrite(IN_B1, LOW);
+    digitalWrite(IN_B1, HIGH);
     digitalWrite(IN_B2, LOW);
-  } while (ultrasonic01.convert(ultrasonic01.timing(), Ultrasonic::CM) <= 10);
+    Serial.println("frente");
+  }
+  if (ult01 < 10) {
+    if (ult02 < 10) {
+      int ult01 = ultrasonic01.convert(ultrasonic01.timing(), Ultrasonic::CM);
+      int ult02 = ultrasonic02.convert(ultrasonic02.timing(), Ultrasonic::CM);
+      digitalWrite(IN_A1, HIGH);
+      digitalWrite(IN_A2, LOW);
+      digitalWrite(IN_B1, LOW);
+      digitalWrite(IN_B2, LOW);
+      Serial.println("entrei no 1");
+    }
+    if (ult03 < 10) {
+      int ult01 = ultrasonic01.convert(ultrasonic01.timing(), Ultrasonic::CM);
+      int ult03 = ultrasonic03.convert(ultrasonic03.timing(), Ultrasonic::CM);
+      digitalWrite(IN_A1, LOW);
+      digitalWrite(IN_A2, LOW);
+      digitalWrite(IN_B1, LOW);
+      digitalWrite(IN_B2, HIGH);
+      Serial.println("entrei no 2");
+    }
+  }
 }
